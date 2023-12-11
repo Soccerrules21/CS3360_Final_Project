35df42034293e8ca0ca4ba4fa9059e20f7426034
ikixu <147871408+ikixu@users.noreply.github.com>
2023-12-11 12:38:50 -0600

    Update GameViewManager.java

--- GameViewManager.java
+++ GameViewManager.java
@@ -1,6 +1,7 @@
 package view;
 
 import javafx.animation.*;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,18 +10,29 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import java.util.Objects;
-import java.util.Random;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import model.menuButtons;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+import static javafx.scene.paint.Color.*;
 
 public class GameViewManager
 {
     private AnchorPane gamePane;
     private Scene gameScene;
     private Stage gameStage;
+
     private static final int GAME_WIDTH = 800;
     private static final int GAME_HEIGHT = 600;
     private MediaPlayer mediaPlayer;
@@ -30,8 +42,8 @@ public class GameViewManager
     ImageView playerView = new ImageView(player);
     Image rock = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/player/rock.png")));
     ImageView rockView = new ImageView(rock);
-    Image coin = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/player/Coin.png")));
-    ImageView Coinview = new ImageView(coin);
+    Image coin = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/player/coin.png")));
+    ImageView coinView = new ImageView(coin);
 
     private boolean isSpacePressed;
     private AnimationTimer gameTimer;
@@ -39,10 +51,9 @@ public class GameViewManager
     private ImageView[] rocks;
     private ImageView[] coins;
     Random randomPositionGenerator;
-    Random randomPositionGenerator2;
 
-    private double rockSpeed = 4;
-    private double coinSpeed = 4;
+    private double rockSpeed = 2;
+    private double coinSpeed = 2;
 
     private final static int PLAYER_RADIUS = 20;
     private final static int ROCK_RADIUS = 20;
@@ -51,25 +62,21 @@ public class GameViewManager
     private long score = 0;
     private Label scoreLabel;
     private long CoinCount = 0;
-    private Label label;
+    private Label coinLabel;
+    List<menuButtons> MENUBUTTONS;
 
 
-
-    public GameViewManager()
-    {
+    public GameViewManager() {
         initializeStage();
         createKeyListeners();
         randomPositionGenerator = new Random();
-        randomPositionGenerator2 = new Random();
     }
 
-    private void createKeyListeners()
-    {
+    private void createKeyListeners() {
         gameScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                if(event.getCode() == KeyCode.SPACE)
-                {
+                if(event.getCode() == KeyCode.SPACE) {
                     isSpacePressed=true;
                 }
             }
@@ -78,40 +85,39 @@ public class GameViewManager
         gameScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                if(event.getCode() == KeyCode.SPACE)
-                {
+                if(event.getCode() == KeyCode.SPACE) {
                     isSpacePressed=false;
                 }
             }
         });
     }
 
-    private void initializeStage()
-    {
+    private void initializeStage() {
         gamePane = new AnchorPane();
         gameScene = new Scene(gamePane, GAME_WIDTH, GAME_HEIGHT);
         gameStage = new Stage();
         gameStage.setScene(gameScene);
-
-
     }
 
-    public void createNewGame(Stage menuStage)
-    {
+    public void createNewGame(Stage menuStage) {
         this.menuStage = menuStage;
         this.menuStage.hide();
         createBackground();
         createPlayer();
         createGameElements();
-        createGameCoins();
         createGameLoop();
+
         scoreLabel = new Label("Score: 0");
-        scoreLabel.setLayoutX(675);
+        scoreLabel.setFont(Font.font("dogica"));
+        scoreLabel.setTextFill(WHITE);
+        scoreLabel.setLayoutX(625);
         scoreLabel.setLayoutY(25);
-        label = new Label("Coins: ");
-        label.setLayoutX(600);
-        label.setLayoutY(25);
-        gamePane.getChildren().addAll(scoreLabel, label);
+        coinLabel = new Label("Coins: ");
+        coinLabel.setFont(Font.font("dogica"));
+        coinLabel.setTextFill(WHITE);
+        coinLabel.setLayoutX(500);
+        coinLabel.setLayoutY(25);
+        gamePane.getChildren().addAll(scoreLabel, coinLabel);
         gameStage.show();
     }
 
@@ -174,15 +180,11 @@ public class GameViewManager
             setNewElementPosition(rocks[i]);
             gamePane.getChildren().add(rocks[i]);
         }
-    }
 
-    private void createGameCoins()
-    {
         coins = new ImageView[4];
-        for(int i = 0; i < coins.length; i++)
-        {
+        for(int i = 0; i < coins.length; i++) {
             coins[i] = new ImageView(coin);
-            setNewCoinPosition(coins[i]);
+            setNewElementPosition(coins[i]);
             gamePane.getChildren().add(coins[i]);
         }
     }
@@ -191,12 +193,7 @@ public class GameViewManager
         for (int i = 0; i < rocks.length; i++) {
             rocks[i].setX(rocks[i].getX() - rockSpeed);
         }
-    }
-
-    private void moveGameCoins()
-    {
-        for(int i = 0; i < coins.length; i++)
-        {
+        for(int i = 0; i < coins.length; i++) {
             coins[i].setX(coins[i].getX() - coinSpeed);
         }
     }
@@ -206,38 +203,21 @@ public class GameViewManager
             if(rocks[i].getX() + rocks[i].getFitWidth() < -100)
                 setNewElementPosition(rocks[i]);
         }
-    }
-
-    private void checkCoins()
-    {
-        for (int i = 0; i < coins.length; i++)
-        {
+        for (int i = 0; i < coins.length; i++) {
             if (coins[i].getX() + coins[i].getFitWidth() < -100)
-            {
-                setNewCoinPosition(coins[i]);  ;
-            }
+                setNewElementPosition(coins[i]);
         }
     }
 
     private void setNewElementPosition(ImageView image) {
-
         int minY = 50;
         int maxY = 500;
         image.setY(-randomPositionGenerator.nextInt(maxY - minY) + maxY);
         image.setX(randomPositionGenerator.nextInt(GAME_WIDTH) + 800);
     }
 
-    private void setNewCoinPosition(ImageView image)
-    {
-        int minY = 50;
-        int maxY = 500;
-        image.setY(-randomPositionGenerator2.nextInt(maxY - minY) + maxY);
-        image.setX(randomPositionGenerator2.nextInt(GAME_WIDTH) + 800);
-    }
-
-    private void createPlayer()
-    {
-        playerView.setPreserveRatio(true);
+    private void createPlayer() {
+        playerView.setPreserveRatio(true); //had to scale the png since it was 16x16
         playerView.setFitHeight(48);
         playerView.setFitWidth(48);
         playerView.setX(GAME_WIDTH-750);
@@ -245,41 +225,21 @@ public class GameViewManager
         gamePane.getChildren().add(playerView);
     }
 
-    private void movePlayer() {
-        int newY = 0;
-
-        if (isSpacePressed) {
-            newY = (int) (playerView.getY() - 4);
-        } else {
-            newY = (int) (playerView.getY() + 4);
-        }
-
-        int minY = 50;
-        int maxY = 500;
-
-        if (newY >= minY && newY <= maxY) {
-            playerView.setY(newY);
-        }
-    }
-
-    private void createGameLoop() {
+    private void createGameLoop() { //constantly updating movement and checking for collision, the further the game progresses, the faster the game will inscrease
         gameTimer = new AnimationTimer() {
 
             @Override
             public void handle (long now) {
                 movePlayer();
                 moveGameElements();
-                moveGameCoins();
                 checkElements();
-                checkCoins();
                 checkCollision();
-                checkCollisionCoin();
                 updateScoreLabel();
                 updateCoins();
 
-                if (frameCounter % 600 == 0)
-                {
-                    rockSpeed = rockSpeed + 1;
+                if (frameCounter % 1500 == 0) {
+                    rockSpeed = rockSpeed + .25;
+                    coinSpeed = coinSpeed + .25;
                 }
                 frameCounter++;
             }
@@ -287,42 +247,78 @@ public class GameViewManager
 
         gameTimer.start();
     }
+    private void movePlayer() {
+        int newY = 0;
 
+            if (isSpacePressed) {
+                newY = (int) (playerView.getY() - 2);
+            } else {
+                newY = (int) (playerView.getY() + 2);
+            }
+
+            int minY = 50;
+            int maxY = 500;
+
+            if (newY >= minY && newY <= maxY) {
+                playerView.setY(newY);
+            }
+    }
     private void checkCollision()
     {
-        for(int i = 0; i < rocks.length; i++){
+        for(int i = 0; i < rocks.length; i++){  //checking collision for rocks; if true: game will "freeze", display "game over" and two buttons; menu and retry
             if ((PLAYER_RADIUS + ROCK_RADIUS) > calculateDistance(playerView.getX() , rocks[i].getX() , playerView.getY() , rocks[i].getY() )){
-                gameStage.close();
                 gameTimer.stop();
-                menuStage.show();
+
+                Text gg = new Text("GAME OVER");
+                gg.setX(240);
+                gg.setY(225);
+                gg.setFont(Font.font("dogica", FontWeight.BOLD, FontPosture.REGULAR, 35));
+                gg.setFill(RED);
+
+                MENUBUTTONS = new ArrayList<>();
+                menuButtons retryButton = new menuButtons("RETRY"); //retry button will start a new game
+                retryButton.setLayoutX(300);
+                retryButton.setLayoutY(375 + MENUBUTTONS.size() * 100);
+                retryButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        gameStage.close();
+                        GameViewManager gameManager = new GameViewManager();
+                        gameManager.createNewGame(gameStage);
+                    }
+                });
+                menuButtons menuButton = new menuButtons("MENU"); //menu button will take you back to the main menu and close the game stage
+                menuButton.setLayoutX(300);
+                menuButton.setLayoutY(275 + MENUBUTTONS.size() * 100);
+                menuButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        gameStage.close();
+                        menuStage.show();
+                    }
+                });
+                gamePane.getChildren().addAll(gg, retryButton, menuButton);
             }
         }
-    }
 
-    private void checkCollisionCoin()
-    {
-        for(int i = 0; i < coins.length; i++)
-        {
-            if((PLAYER_RADIUS + COIN_RADIUS) > calculateDistance(playerView.getX() , coins[i].getX(), playerView.getY(), coins[i].getY() ))
-            {
+        for(int i = 0; i < coins.length; i++){ //checking collision for coins; if true: coin will disappear and play a sound, counter will increase by one
+            if((PLAYER_RADIUS + COIN_RADIUS) > calculateDistance(playerView.getX() , coins[i].getX(), playerView.getY(), coins[i].getY())) {
                 CoinCount++;
-                setNewCoinPosition(coins[i]);
+                setNewElementPosition(coins[i]);
                 CoinSound();
             }
         }
     }
 
-    void CoinSound()
-    {
-        String Musicfile = "/player/CollectCoin.mp3";
+    private void CoinSound() {
+        String Musicfile = "/player/collectcoin-6075.mp3";
         Media sound2 = new Media(Objects.requireNonNull(getClass().getResource(Musicfile)).toString());
         mediaPlayer = new MediaPlayer(sound2);
         mediaPlayer.setVolume(1.0);
         mediaPlayer.play();
     }
 
-    private double calculateDistance(double x1, double x2, double y1, double y2)
-    {
+    private double calculateDistance(double x1, double x2, double y1, double y2){  //distance formula, used for collision
         return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
     }
 
@@ -332,8 +328,7 @@ public class GameViewManager
             scoreLabel.setText("Score: " + score);
         }
     }
-    void updateCoins()
-    {
-        label.setText("Coins: " + CoinCount);
+    private void updateCoins() {
+        coinLabel.setText("Coins: " + CoinCount);
     }
 }
